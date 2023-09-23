cdeee3701e6c150d4538c51492f8ea6091c43851
Abhishek Vats <virus@Abhisheks-MacBook-Air.local>
2023-09-23 18:05:51 +0530

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,41 +1,73 @@
-import React, {  useState,useEffect } from 'react'
-import "./styles.scss"
-import { FaQuoteLeft, FaTwitterSquare } from "react-icons/fa";
-import useGenerateRandomColor from './components/useGenerateRandomColor';
-import Quote from './components/Quote';
+import React, { useState, useEffect } from "react";
+import "./styles.scss";
+import { FaTwitterSquare } from "react-icons/fa";
+import useGenerateRandomColor from "./components/useGenerateRandomColor";
+import Quote from "./components/Quote";
+import axios from "axios";
+import { SkeletonTheme } from "react-loading-skeleton";
 
 const App = () => {
+  const { color, generateColor } = useGenerateRandomColor();
+  useEffect(() => {
+    const root = document.documentElement;
+    root.style.setProperty("--user-color", color);
+  }, [color]);
 
-    const { color, generateColor } = useGenerateRandomColor();
-    useEffect(() => {
-        const root = document.documentElement;
-        root.style.setProperty('--user-color', color);
-    }, [color]);
+  const options = {
+    method: "GET",
+    url: "https://quotes15.p.rapidapi.com/quotes/random/",
+    headers: {
+      "X-RapidAPI-Key": `${process.env.REACT_APP_RAPIDAPI_KEY}`,
+      "X-RapidAPI-Host": "quotes15.p.rapidapi.com",
+    },
+    // get the evn key from here https://rapidapi.com/martin.svoboda/api/quotes15/
+  };
 
-    return (
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const handleClick = async () => {
+    generateColor();
+    setIsLoading(true);
+    try {
+      const response = await axios.request(options);
+      setQuote(response.data.content);
+      setAuthor(response.data.originator.name);
+    } catch (error) {
+      console.error(error);
+    }
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    handleClick();
+  }, []);
 
-        <div>
-            <div id="quote-box">
-                <div id="text">
-                    <Quote />
-                </div>
-                
-                <div className="buttons">
-                    <a href='https://twitter.com/intent/tweet' >
-                        <button id='tweet' style={{ display: 'none' }}>Twitter</button>
-                        <label htmlFor='twitter'>
-                            <FaTwitterSquare className='twitter' />
-                        </label>
-                    </a>
-                    <button id="new-quote" onClick={generateColor}>
-                        New Quote
-                    </button>
-                </div>
-            </div>
-            <p style={{ color: '#fff', textAlign: 'center' }}>By Virus</p>
+  return (
+    <div>
+      <div id="quote-box">
+        <div id="text">
+          <SkeletonTheme color="#202020" highlightColor={color}>
+            <Quote quote={quote} author={author} isLoading={isLoading} />
+          </SkeletonTheme>
         </div>
-    )
-}
 
+        <div className="buttons">
+          <a href="https://twitter.com/intent/tweet">
+            <button id="tweet" style={{ display: "none" }}>
+              Twitter
+            </button>
+            <label htmlFor="twitter">
+              <FaTwitterSquare className="twitter" />
+            </label>
+          </a>
+          <button id="new-quote" onClick={handleClick}>
+            New Quote
+          </button>
+        </div>
+      </div>
+      <p style={{ color: "#fff", textAlign: "center" }}>By Virus</p>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,40 +1,28 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import { FaQuoteLeft } from "react-icons/fa";
 
+const Quote = ({ quote, author, isLoading }) => {
+  return (
+    <div>
+      <FaQuoteLeft style={{ marginRight: "10px" }} />
+      <div
+        style={{
+          overflow: "scroll",
+          maxHeight: "300px",
+        }}
+      >
+        {isLoading ? (
+          <Skeleton count={3} width="100%" height="0.8rem" />
+        ) : (
+          quote
+        )}
+      </div>
+      <div id="author">
+        <p>- {isLoading ? <Skeleton height="0.5rem" /> : author}</p>
+      </div>
+    </div>
+  );
+};
 
-const Quote = () => {
-    const [quote, setQuote] = useState("");
-    const [author, setAuthor] = useState("");
-
-    // copied from rapidapi https://rapidapi.com/martin.svoboda/api/quotes15/
-    const options = {
-        method: 'GET',
-        url: 'https://quotes15.p.rapidapi.com/quotes/random/',
-        headers: {
-            'X-RapidAPI-Key': `${process.env.REACT_APP_RAPIDAPI_KEY}`,
-            'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
-        }
-    };
-    
-    axios.request(options).then(function (response) {
-        setQuote(response.data.content);
-        setAuthor(response.data.originator.name);
-
-    }).catch(function (error) {
-        console.error(error);
-    });
-
-    return (
-        <div>
-            <FaQuoteLeft style={{ marginRight: '10px' }} />
-            {quote}
-            <div id="author">
-                <p>- {author}</p>
-            </div>
-        </div>
-
-    )
-}
-
-export default Quote
+export default Quote;
